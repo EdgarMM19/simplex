541eb1d3370b3a01661fe4352d258226563a33ad
EdgarMM19 <48213850+EdgarMM19@users.noreply.github.com>
2019-10-10 12:00:16 +0200

    Update Matrix.hh

--- Matrix.hh
+++ Matrix.hh
@@ -1,3 +1,44 @@
 // Definition of class Matrix
 //supports matrix, vector
 //multiplication,inverse,sum,minimum and maximum search with position
+
+
+#ifndef _MATRIX_
+#define _MATRIX_
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <Rational>
+
+/*
+ * Clase Matrix
+ */
+class Matrix {
+
+
+private:
+
+  vector<vector<Rational>> M;
+
+public:
+
+  Matrix();
+  Matrix(int n, int m);
+
+  Matrix override operator+(const Matrix& a) const;
+
+  Matrix override operator*(const Matrix& a) const;
+  Matrix override operator-(const Matrix& a) const;
+  Matrix inverse() const;
+  Matrix override operator==(const Matrix& a) const;
+  Matrix override operator!=(const Matrix& a) const;
+
+  Matrix override operator>(const Matrix& a) const;
+
+  Matrix override operator<(const Matrix& a) const;
+
+};
+
+#endif
+
